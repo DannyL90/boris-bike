defb71233dede44af7161e70044bb652d846bda0
Daniel <daniel.szabo09@gmail.com>
2021-09-22 17:11:56 +0100

    After thired pair programming.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,21 @@
+
 class DockingStation
-    def initialize
+    attr_reader :bike
+    def initialize(bike = Bike.new)
+        @bikes_in = []
+        @bike = bike
     end
 
-    def release_bike
-
+    def release_bike(bike)
+        @bikes_in == [] ? (raise "Can not take bike, dock is empty") : (@bikes_in.delete(bike))
     end
 
+    def docking_bike(bike)
+        @bikes_in.push(bike)
+    end
+
+    def show_bikes
+        @bikes_in
+    end
 end
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,15 @@
 require "./lib/docking_station.rb"
 
 
-# describe DockingStation do
-#     it "Docking Station releases bike" do
-#       expect(DockingStation.new).to respond_to(:release_bike)
-#     end 
-# end
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
-  end
-
-
-
+    it { is_expected.to respond_to :docking_bike}
+    it { is_expected.to respond_to :show_bikes}
+    
+    it "dock station empty" do
+      dockingstation = DockingStation.new
+      bike = Bike.new
+      expect{dockingstation.release_bike(bike)}.to raise_error("Can not take bike, dock is empty")
+    end
+end
